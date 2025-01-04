4ac4bbd6351b7f7e953cb6c9daaf4ebe266a22cb
Dex <nguyenducduy1112006@gmail.com>
2025-01-04 14:25:54 +0700

    ShopGUI update

---
+++ Assets/_Update/Day 2 - 31.12.2024/Scripts/ShopScript/ChoseProduct.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoseProduct : MonoBehaviour
+{
+    public GameObject ProductTag;
+    public Text ProductName;
+    public Text ProductNameView;
+    public Text PriceOfProduct;
+    public Image ImageOfProduct;
+    public Sprite SpriteForImageOfProduct;
+    public int ProductPrice;
+    private string price;
+    public InputField AmoutOfProduce;
+    private int TotalAmount;
+    public Text TotalProductCost;
+    private int totalCost;
+    private string toStringTotalCost;
+    public Text ProductDesc;
+    public GameObject ProductPreviewPanel;
+    public void Update(){
+        if(ProductTag.tag == "Product1"){
+            ProductPrice = 10;
+        }else if(ProductTag.tag == "Product2"){
+            ProductPrice = 100;
+        }
+
+        if (int.TryParse(AmoutOfProduce.text, out TotalAmount))
+        {
+            totalCost = TotalAmount * ProductPrice;
+            toStringTotalCost = totalCost.ToString();
+            TotalProductCost.text = "Total Cost: $" + $"{toStringTotalCost}";
+        }else
+        {
+            TotalProductCost.text = "Total Cost: $0"; // Default for invalid input
+        }
+    }
+    public void OnButtonClick(){
+        ProductPreviewPanel.SetActive(true);
+        if(ProductTag.tag == "Product1"){
+            price = ProductPrice.ToString();
+            ProductDesc.text = "Want more cafein???";
+            ProductNameView.text = $"{ProductName.text}";
+            PriceOfProduct.text = $"${price}";
+            ImageOfProduct.sprite = SpriteForImageOfProduct;
+            TotalAmount = int.Parse(AmoutOfProduce.text);
+            TotalProductCost.text = "Total Cost: " + $"${TotalAmount*ProductPrice}";
+        }
+        else if(ProductTag.tag == "Product2"){
+            price = ProductPrice.ToString();
+            ProductDesc.text = "Is it? A Humannn?";
+            ProductNameView.text = $"{ProductName.text}";
+            PriceOfProduct.text = $"${price}";
+            ImageOfProduct.sprite = SpriteForImageOfProduct;
+            TotalAmount = int.Parse(AmoutOfProduce.text);
+            TotalProductCost.text = "Total Cost: " + $"${TotalAmount*ProductPrice}";
+        }
+    }
+    
+}
