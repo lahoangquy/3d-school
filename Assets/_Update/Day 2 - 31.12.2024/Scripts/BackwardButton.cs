b1c1c4d1426d71f1db99697c8431542547eaa7e7
Dex <nguyenducduy1112006@gmail.com>
2025-01-13 22:17:44 +0700

    Enrollment GUI Improve

--- Assets/_Update/Day 2 - 31.12.2024/Scripts/BackwardButton.cs
+++ Assets/_Update/Day 2 - 31.12.2024/Scripts/BackwardButton.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackwardButton : MonoBehaviour
 {
     public GameObject BackwardButton1;
+    public GameObject Forwardbutton;
     public GameObject UniversityUI;
     public GameObject DegreeUI;
     public GameObject HouseUI;
+    public GameObject Tuition;
+    //Icon Section
+
+    public Button UniIcon;
+    public Button DegreeIcon;
+    public Button HouseIcon;
+    public Button TuitionIcon;
 
     public void OnButtonClick()
     {
-        if (DegreeUI.activeSelf)
+        if (UniversityUI.activeSelf){
+            Forwardbutton.SetActive(true);
+        }else if (DegreeUI.activeSelf)
         {
             UniversityUI.SetActive(true);
             BackwardButton1.SetActive(false);
-            DegreeUI.SetActive(false);            
+            DegreeUI.SetActive(false);
+            Forwardbutton.SetActive(true);    
+            DegreeIcon.GetComponent<Button>().interactable = false;  
+            UniIcon.GetComponent<Button>().interactable = true;      
         }else if (HouseUI.activeSelf)
         {
             DegreeUI.SetActive(true);
             HouseUI.SetActive(false);
+            HouseIcon.GetComponent<Button>().interactable = false;
+            DegreeIcon.GetComponent<Button>().interactable = true;
+        }else if (Tuition.activeSelf){
+            Tuition.SetActive(false);
+            HouseUI.SetActive(true);
+            Forwardbutton.SetActive(true);
+            HouseIcon.GetComponent<Button>().interactable = true;
+            TuitionIcon.GetComponent<Button>().interactable = false; 
         }
     } 
 }
--- Assets/_Update/Day 2 - 31.12.2024/Scripts/PlayerScript/PlayerGetJob.cs
+++ Assets/_Update/Day 2 - 31.12.2024/Scripts/PlayerScript/PlayerGetJob.cs
@@ -6,11 +6,13 @@ using DialogueEditor;
 public class PlayerGetJob : MonoBehaviour
 {
     public GameObject Item;
+    public NPCConversation MrWhite;
 
     public void AcceptJob()
     {
         if (!Item.activeSelf){
             Item.SetActive(true);
+            ConversationManager.Instance.SetBool("takenItem",true);
         }
     }
 }
